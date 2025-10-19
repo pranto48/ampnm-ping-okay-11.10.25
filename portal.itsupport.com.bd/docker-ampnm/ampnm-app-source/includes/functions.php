c8a48df9a9e62dc2267f45567d8073c9d1e7b04f
[dyad] <pranto48@gmail.com>
2025-10-19 16:04:08 +0600

    [dyad] Improved application loading speed by decoupling license revalidation from the initial page load and making API endpoints purely session-based. - wrote 3 file(s) + extra files edited outside of Dyad

--- portal.itsupport.com.bd/docker-ampnm/ampnm-app-source/includes/functions.php
+++ portal.itsupport.com.bd/docker-ampnm/ampnm-app-source/includes/functions.php
@@ -55,7 +55,7 @@ function generateLicenseKey($prefix = 'AMPNM') {
 function generateUuid() {
     $data = random_bytes(16);
     $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
-    $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+    $data[8] = chr(ord(ord($data[8]) & 0x3f | 0x80)); // set bits 6-7 to 10
     return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
 }
 
@@ -696,6 +696,7 @@ function admin_header($title = "Admin Panel") {
     
     $admin_nav_links = [
         'index.php' => 'Dashboard',
+        'orders.php' => '<i class="fas fa-shopping-bag mr-1"></i> Orders', // NEW ORDERS LINK
         'users.php' => 'Customers',
         'license-manager.php' => 'Licenses',
         'products.php' => 'Products',
